c086213d4fe336280f2be174d8f26c0d2b8aa7d3
Rodrigo Moya <rodrigo.moya@xamarin.com>
2018-05-31 16:57:21 +0200

    [BuildOutput] Use typed metadata for counters

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputWidget.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputWidget.cs
@@ -636,7 +636,7 @@ namespace MonoDevelop.Ide.BuildOutputView
 				await SetSpinnerVisibility (true);
 
 				try {
-					var metadata = new Dictionary<string, string> ();
+					var metadata = new BuildOutputCounterMetadata ();
 					using (Counters.ProcessBuildLog.BeginTiming (metadata)) {
 						BuildOutput.ProcessProjects (showDiagnostics, metadata);
 
